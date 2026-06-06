6c57bf6fdad68003917d251a9f9966876896b43e
Navneet <navneetmui@gmail.com>
2026-06-06 22:55:45 +0530

    Initial Commit

---
+++ src/main/java/com/juvenile/docScale/DocScaleApplication.java
@@ -0,0 +1,13 @@
+package com.juvenile.docScale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocScaleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocScaleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/juvenile/docScale/DocScaleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juvenile.docScale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocScaleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
